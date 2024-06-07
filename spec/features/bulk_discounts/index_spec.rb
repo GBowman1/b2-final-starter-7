55b390b60966e8332bd59925288d22a572e1b389
Garrett Bowman <garrettbowman248@gmail.com>
2024-06-07 07:21:21 -0500

    Finished User Story 1

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.describe "bulk discounts index page" do
     describe "as a merchant" do
         # US 1 part 2
-        scenario "when I visit the bulk discounts index page" do
+        describe "when I visit the bulk discounts index page" do
             it "I can see all of my bulk discounts including their percentage discount and quantity thresholds" do
-                merchant = Merchant.create!(name: "Kermit The Frog")
+                merchant = Merchant.create!(name: "Amazon")
                 bulk_discount_1 = merchant.bulk_discounts.create!(percentage_discount: 10, quantity_threshold: 10)
                 bulk_discount_2 = merchant.bulk_discounts.create!(percentage_discount: 20, quantity_threshold: 20)
                 bulk_discount_3 = merchant.bulk_discounts.create!(percentage_discount: 30, quantity_threshold: 30)
@@ -13,8 +13,8 @@ RSpec.describe "bulk discounts index page" do
                 visit "/merchants/#{merchant.id}/bulk_discounts"
 
                 within("#index_table") do
-                expect(page).to have_content("Bulk Discounts")
-                expect(page).to have_content("Percentage Discount")
+                expect(page).to have_content("Discount ID")
+                expect(page).to have_content("Discount Percentage")
                 expect(page).to have_content("Quantity Threshold")
                 expect(page).to have_content("Links")
                 expect(page).to have_content(bulk_discount_1.percentage_discount)
@@ -26,19 +26,18 @@ RSpec.describe "bulk discounts index page" do
                 end
             end
             it "each bulk discount listed includes a link to its show page" do
-                merchant = Merchant.create!(name: "Kermit The Frog")
+                merchant = Merchant.create!(name: "Amazon")
                 bulk_discount_1 = merchant.bulk_discounts.create!(percentage_discount: 10, quantity_threshold: 10)
                 bulk_discount_2 = merchant.bulk_discounts.create!(percentage_discount: 20, quantity_threshold: 20)
 
                 visit "/merchants/#{merchant.id}/bulk_discounts"
-                within("#index_table") do
-                    within("#link_#{bulk_discount_1.id}") do
-                    expect(page).to have_link("View")
+                
+                within("#link_#{bulk_discount_1.id}") do
+                expect(page).to have_link("View")
 
-                    click_link("View")
-                    expect(current_path).to eq("/merchants/#{merchant.id}/bulk_discounts/#{bulk_discount_1.id}")
-                    expect(current_path).to_not eq("/merchants/#{merchant.id}/bulk_discounts/#{bulk_discount_2.id}")
-                    end
+                click_link("View")
+                expect(current_path).to eq("/merchants/#{merchant.id}/bulk_discounts/#{bulk_discount_1.id}")
+                expect(current_path).to_not eq("/merchants/#{merchant.id}/bulk_discounts/#{bulk_discount_2.id}")
                 end
             end
         end
